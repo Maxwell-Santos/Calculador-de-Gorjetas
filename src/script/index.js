c5d5f82bf7fd7a51e31343db0e3cd393b8bcd71f
Maxwell <max.coding011@gmail.com>
2022-04-18 12:29:10 -0300

    atualização em tempo real das porcentagens, tabém ja foi finalizado, agora falta fazer a porcentagem customizada e ativar o button resete de acordo com o algoritmo

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,3 @@
-const input = document.querySelector('#one');
-const input2 = document.querySelector('#two');
-
-const reset = document.querySelector('.reset')
-
-
 const porcetagens = document.querySelectorAll('.porcent-item')
 
 const porcentArray = Array.from(porcetagens)
@@ -15,3 +9,29 @@ porcentArray.forEach(item =>{
     console.log(item)
   })
 })
+
+
+//==============CALC LOGIC ==============
+
+const price = document.querySelector('#one') //price of bill
+const persons = document.querySelector('#two') //number of peoples
+
+const tip_person_amount = document.querySelector('#tip-person')
+const total_person = document.querySelector('#total-person')
+
+
+function calcPorcentage(porcent){ //calc porcentage of tip
+
+  const newPorcent = porcent / 100 //translate porcentage to decimal
+
+  const tot = (price.value * newPorcent) / persons.value //calc tip for person
+  tip_person_amount.innerHTML = tot.toFixed(2)
+
+  //calcTotal
+  const seila = (price.value / persons.value) + tot
+
+  total_person.innerHTML = seila.toFixed(2)
+  
+}
+
+// os item tem a mesma função essa calcPorcent, só que quando clica em cada uma, ela joga um parâmetro diferente, a conta da porcentagem faz com esse parâmetro, tendeu? um toque pra sim, dois pra n KKKKKKKKK
--- src/script/input-focus.js
+++
@@ -1,8 +0,0 @@
-
-
-const entrada1 = document.querySelector('#one');
-const entrada2 = document.querySelector('#two');
-
-const content_entrada1 = document.querySelector('.bill-input');
-const content_entrada2 = document.querySelector('.people-input');
-
