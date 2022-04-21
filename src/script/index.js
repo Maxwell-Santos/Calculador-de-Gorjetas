c4b57cadd45d49f996240fda924a26f0308b469f
Maxwell <max.coding011@gmail.com>
2022-04-20 23:09:33 -0300

    correções para os possíveis erros do usuário e animação de ao carregar a pagina e responsividade

--- src/script/index.js
+++ src/script/index.js
@@ -1,20 +1,30 @@
-window.onload = () => {
-  alert('Caso queira uma porcentagem da sua preferência, basta digitar o valor na área "Custom" e clicar Enter')
-}
-
-//==============CALC LOGIC ==============
 
 const price = document.querySelector('#one') //price of bill
 const persons = document.querySelector('#two') //number of peoples
 const custom_porcent = document.querySelector('#custom-porcent')
 
-const tip_person_amount = document.querySelector('#tip-person')
-const total_person = document.querySelector('#total-person')
+const tip_person_amount = document.querySelector('#tip-person') //show the result of calc for tip
+const total_person = document.querySelector('#total-person') // show the result of calc in total
 
 const reset = document.querySelector('.reset')
 
+window.onload = () => {
 
-reset.addEventListener('click', () => { //resetar a aplicação
+  setTimeout(() => {
+    price.focus();
+    alert('Caso queira uma porcentagem da sua preferência, basta digitar o valor na área "Custom" e clicar Enter');
+
+  }, 2000)
+}
+
+function controlBorders() {
+  price.classList.add('active') //active danger border
+  persons.classList.add('active') //active danger border
+  custom_porcent.classList.add('active') //active danger border
+
+}
+
+reset.addEventListener('click', () => { //reset the aplication
   price.value = ''
   persons.value = ''
   custom_porcent.value = ''
@@ -30,24 +40,39 @@ reset.addEventListener('click', () => { //resetar a aplicação
 })
 
 
-function digitar(event) { 
-  var x = event.keyCode; // 
+function OnClickEnter(event) {
+  var x = event.keyCode;
 
   if (x == 13) {
 
-    if (price.value == '' && persons.value == '' && custom_porcent.value == '') { //se os input estiverem vazios, alert(), se não, mostrar o resultado na tela
+    //se os campos obrigátórios n estiverem preenchido, faça isso
+    //if the required fields are not filled, do this
+    if (price.value == '' && persons.value == '' && custom_porcent.value == '') {
+
       alert('Preencha os campos')
+      controlBorders();
 
-      price.classList.toggle('active') //active danger border
-      persons.classList.toggle('active') //active danger border
-      custom_porcent.classList.toggle('active') //active danger border
 
+    } else if(price.value == '' && persons.value !== '' && custom_porcent.value == ''){
+      alert('Preencha o campo Bill')
+      price.focus();
+
+
+    } else if (price.value !== '' && persons.value == '' && custom_porcent.value == '') {
+      alert('Preencha o campo Number of People')
+      persons.focus();
+      
+      //se não, se os campos obrigatórios estiverem preenchidos mas o usuário não definiu uma porcentagem, faça isso
+      //if not, if the required fields are filled in but the user has not set a percentage, do so
     } else if (price.value !== '' && persons.value !== '' && custom_porcent.value == '') {
-      alert('Escolha uma porcentagem, ou se preferir pode definir um valor da sua preferência')
+
+      alert('Escolha uma porcentagem, ou defina um valor da sua preferência')
       custom_porcent.focus()
 
+
     } else {
-      if (custom_porcent.value > 100) {
+
+      if (custom_porcent.value > 100) {//o valor máximo da porcentagem e 100%
 
         alert('O valor máximo é 100')
 
@@ -55,34 +80,45 @@ function digitar(event) {
         custom_porcent.focus()
 
       } else {
-        customPercentage(custom_porcent.value)
+        customPercentage(custom_porcent.value) //ele joga como parâmetro o valor digitaoo
 
       }
+
     }
   }
 }
 
+
+//essa função recebe como parâmetro o valor digitado, assim como mostra na linha 83
+//this function receives the value entered as a parameter, as shown in line 83
 function customPercentage(customPorcent) {
+  //esse valor digitado, será jogado como parâmetro para o calculo
+  //the entered value, will be used how param to the calc
   calcPercentage(customPorcent)
 
 }
 
-
-function show(tot, seila) { // this function show the result on screen
-  tip_person_amount.innerHTML = tot.toFixed(2) //limits decimal places to two places
-  total_person.innerHTML = seila.toFixed(2) //same thing to total_person
+function show(tipForPerson, totalforPerson) { // this function show the result on screen
+  tip_person_amount.innerHTML = tipForPerson.toFixed(2) //limits decimal places to two places
+  total_person.innerHTML = totalforPerson.toFixed(2) //same thing to total_person
 
 }
 
-
 function calcPercentage(porcent) { //calc porcentage of tip
+
   const newPorcent = porcent / 100 //translate porcentage to decimal
 
-  const tot = (price.value * newPorcent) / persons.value //calc tip for person  
-  const seila = (price.value / persons.value) + tot
+  const tipForPerson = (price.value * newPorcent) / persons.value //calc tip for person  
+  const totalforPerson = (price.value / persons.value) + tipForPerson
 
-  show(tot, seila)
+  if (price.value == '' || persons.value == '') {
+    alert('preencha os campos')
 
-  reset.disabled = false //active button reset
+    controlBorders();
 
+  } else {
+    show(tipForPerson, totalforPerson)
+    reset.disabled = false //active button reset
+
+  }
 }
